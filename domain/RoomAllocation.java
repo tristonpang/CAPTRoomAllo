8dc5261f4c3804dbe87aeb88327f9c02095c8f51
TRISTON-XPS15\lawll <t.pang288@hotmail.com>
2018-05-29 12:15:20 +0800

    created RoomAllocation class that represents the PlanningSolution

---
+++ domain/RoomAllocation.java
@@ -0,0 +1,20 @@
+//package org.optaplanner.examples.cloudbalancing.domain;
+
+import java.util.List;
+
+import com.thoughtworks.xstream.annotations.XStreamAlias;
+import com.thoughtworks.xstream.annotations.XStreamConverter;
+import org.optaplanner.core.api.domain.solution.PlanningEntityCollectionProperty;
+import org.optaplanner.core.api.domain.solution.PlanningScore;
+import org.optaplanner.core.api.domain.solution.PlanningSolution;
+import org.optaplanner.core.api.domain.solution.drools.ProblemFactCollectionProperty;
+import org.optaplanner.core.api.domain.valuerange.ValueRangeProvider;
+import org.optaplanner.core.api.score.buildin.hardsoft.HardSoftScore;
+import org.optaplanner.examples.common.domain.AbstractPersistable;
+import org.optaplanner.persistence.xstream.api.score.buildin.hardsoft.HardSoftScoreXStreamConverter;
+
+@PlanningSolution
+@XStreamAlias("RoomAllocation")
+public class RoomAllocation extends AbstractPersistable{
+    
+}
